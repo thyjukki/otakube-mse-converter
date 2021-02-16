17e29368274a5799b6db2ddf12749b7638fdbeca
Jussi Joki <judejoki@gmail.com>
2021-02-16 12:22:13 +0200

    First commit

---
+++ card.py
@@ -0,0 +1,55 @@
+class Card():
+
+    def __init__(self, entry):
+      self.id = int(entry[0])
+      self.name = entry[1]
+      self.revision = entry[2]
+      self.type = entry[3]
+      self.subtypes = entry[3]
+      self.color = entry[4]
+      self.cost = entry[5]
+      self.power = None
+      self.toughness = None
+      self.rules_text  = entry[7]
+      self.flavor_text  = entry[9]
+      self.creator  = entry[12]
+
+    def __repr__(self):
+        return f"Card([{self.id, self.name}])"
+
+    def __str__(self):
+        return f"id: {self.id} name: {self.name}"
+
+    def generate_mse_card(self):
+
+      text = f"""
+card:
+	has styling: false
+	notes: 
+	time created: 2019-06-02 01:47:48
+	time modified: 2019-06-02 01:50:10
+	name: {self.name}
+	casting cost: {self.cost}
+	image:
+	super type: <word-list-type>{self.type}</word-list-type>
+	sub type: <word-list-race></word-list-race>
+	rarity: rare
+	rule text:
+		{self.rules_text}		
+	flavor text: <i-flavor>{self.flavor_text}</i-flavor>"""
+      if self.power and self.toughness:
+        text += f"""
+	power: {self.power}
+	toughness: {self.toughness}"""
+
+      text += f"""
+	card code text: 
+	illustrator:
+		{self.creator}
+	copyright: 
+	image 2: 
+	copyright 2: 
+	copyright 3: 
+	mainframe image: 
+	mainframe image 2:"""
+      return text
---
+++ mse_generate.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+from csv import reader
+from typing import List
+from card import Card
+
+
+def parse_list(file):
+	# open file in read mode
+	with open(file, 'r', encoding='UTF-8') as read_obj:
+		csv_reader = reader(read_obj)
+		next(csv_reader)
+		next(csv_reader)
+		cards = [Card(row) for row in csv_reader]
+
+		sorted_cards = sorted(cards, key= lambda e:e.id)
+
+		return sorted_cards
+
+
+def generate_mse_set(cards: List[Card]):
+	with open('mse_header.txt', 'r', encoding='UTF-8') as read_obj:
+		full_text = read_obj.read()
+
+	for card in cards:
+		full_text += card.generate_mse_card()
+	return full_text
+
+
+def run():
+	cards = parse_list('otakube.csv')
+
+	for card in cards:
+		print(card)
+
+	output = generate_mse_set(cards)
+	if not os.path.exists('build'):
+		os.makedirs('build')
+	with open('build/set', 'w', encoding='UTF-8') as file:
+		file.write(output)
+	
+	shutil.make_archive('otakube', 'zip', 'build')
+	os.rename('otakube.zip', 'otakube.mse-set')
+
+if __name__ == '__main__':
+	run()
