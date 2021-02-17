53dee5b43e6f3e4649ccc9269e45ab72f6ca9482
Jussi Joki <judejoki@gmail.com>
2021-02-17 16:50:40 +0200

    add mse with liquid mana configured

---
+++ MSE/data/magic-mana-large-otakube.mse-symbol-font/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic-mana-large-otakube.mse-symbol-font magic-mana-small-otakube.mse-symbol-font magic-mana-large.mse-symbol-font
+
+ren "magic-mana-large-otakube.mse-installer" "Otakube - M15 Style.mse-installer"
